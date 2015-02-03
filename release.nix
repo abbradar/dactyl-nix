3d27e660f5cb069b57cde9a6051e11c435692825
Nikolay Amiantov <ab@fmap.me>
2015-02-03 15:20:54 +0300

    Override src

--- release.nix
+++ release.nix
@@ -9,6 +9,8 @@ genAttrs supportedPlatforms (system:
       inherit system;
     };
   in {
-    pentadactyl = pkgs.callPackage ./pentadactyl.nix { };
+    pentadactyl = overridePackage (pkgs.callPackage ./pentadactyl.nix { }) { args:
+      inherit src;
+    };
   }
 )
