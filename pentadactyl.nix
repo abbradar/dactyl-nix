91e976bfe5c51801ed375e9221c8480de2e16fcb
Nikolay Amiantov <ab@fmap.me>
2016-05-07 16:31:59 +0300

    update, add patch to fix most commands

--- pentadactyl.nix
+++ pentadactyl.nix
@@ -1,23 +1,28 @@
-{ stdenv, fetchgit, firefox, which, zip }:
+{ stdenv, fetchFromGitHub, fetchpatch, firefox, which, zip }:
 
 stdenv.mkDerivation rec {
   name = "pentadactyl";
 
-  src = fetchgit {
-    url = "https://github.com/5digits/dactyl";
-    rev = "4356313a0902efbb34013357235419d9cbffeded";
-    sha256 = "6655e0dfee34add0208c3519b27e12fdc746c0b5493ec4e94403e64c51cb3460";
+  src = fetchFromGitHub {
+    owner = "5digits";
+    repo = "dactyl";
+    rev = "1a4290d92ac3e2dfd3b84168081ba721c02556b0";
+    sha256 = "1yravd4ypzs822q8vg5rqf75fz28ihvxcrlg63shpvyjaq1b0zbw";
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/mikar/dactyl/commit/2f32e55e0f0fcf861371055f237dbe928c7835d7.patch";
+      sha256 = "1w9qhm4sn5f5waajqmwjd8d58a9vy6bz2zjb82wclmjdci77z12m";
+    })
+  ];
 
   nativeBuildInputs = [ which zip ];
 
   buildInputs = [ firefox ];
 
-  postUnpack = ''
-    sourceRoot="$sourceRoot/pentadactyl"
-  '';
-
   postPatch = ''
+    cd pentadactyl
     sed -i 's/maxVersion="[^"]*/maxVersion="50/' install.rdf
   '';
 
